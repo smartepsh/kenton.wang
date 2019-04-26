3ac5109bea2789fe47dd94a2a34defaf672cf747
Kenton Wang <smartepsh@gmail.com>
2019-04-26 16:46:25 +0800

    temp: hello world

---
+++ src/components/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>{{ greeting }} World !</p>
+  </div>
+</template>
+
+<script src="./index.js" />
+
+<style>
+ p {
+   font-size: 2em;
+   text-align: center;
+ }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import Index from './components/index.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+new Vue({
+    // el: '#app',
+    render: h => h(Index)
+}).$mount('#app');
